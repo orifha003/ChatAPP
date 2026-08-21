f8f0ee7af05313ef454dc622454456d1f0df687b
orifha003 <orifha03mulovhedzi@gmail.com>
2026-08-21 16:42:31 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/progapp/ProgAPP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progapp;
+
+/**
+ *
+ * @author orifh
+ */
+public class ProgAPP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
