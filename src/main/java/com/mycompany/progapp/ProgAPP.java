781fe4ae3e7cfbf957b222842b65963f9ffdf762
orifha003 <orifha03mulovhedzi@gmail.com>
2026-08-25 14:18:32 +0200

    update changes

--- src/main/java/com/mycompany/progapp/ProgAPP.java
+++ src/main/java/com/mycompany/progapp/ProgAPP.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.progapp;
-
+import java.util.Scanner;
 /**
  *
  * @author orifh
@@ -11,6 +11,7 @@ package com.mycompany.progapp;
 public class ProgAPP {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello World!");
     }
 }
