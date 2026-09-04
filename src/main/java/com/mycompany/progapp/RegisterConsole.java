d7d8c0f204cb5f8b62f5dc23045b4f41d2b2ce9a
orifha003 <orifha03mulovhedzi@gmail.com>
2026-09-04 21:55:13 +0200

    add comments in RegisterConsole.java

--- src/main/java/com/mycompany/progapp/RegisterConsole.java
+++ src/main/java/com/mycompany/progapp/RegisterConsole.java
@@ -11,51 +11,55 @@ package com.mycompany.progapp;
  */
 public class RegisterConsole {
 
-    // stores the registered details so login can check against them
-    String storedUsername;
-    String storedPassword;
-    String storedCellNumber;
-    String storedFirstName;
-    String storedLastName;
-
-    boolean checkUserName(String username) {
-        boolean hasUnderscore = false;
-        for (int i = 0; i < username.length(); i++) {
-            char c = username.charAt(i);
-            if (c == '_') hasUnderscore = true;
-        }
-        boolean validLength = username.length() <= 5;
-        return hasUnderscore && validLength;
+    // Declarationof variables
+        String storedUsername;
+        String storedPassword;
+        String storedCellNumber;
+        String storedFirstName;
+        String storedLastName;
+    
+    //check username requirements
+        boolean checkUserName(String username) {
+            boolean hasUnderscore = false;
+            for (int i = 0; i < username.length(); i++) {
+                char c = username.charAt(i);
+                if (c == '_') hasUnderscore = true;
+            }
+            boolean validLength = username.length() <= 5;
+            return hasUnderscore && validLength;
     }
-
-    boolean checkPasswordComplexity(String password) {
-        boolean hasUpper = false;
-        boolean hasLower = false;
-        boolean hasDigit = false;
-        boolean hasSpecial = false;
-        for (int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
-            if (Character.isUpperCase(c)) hasUpper = true;
-            if (Character.isLowerCase(c)) hasLower = true;
-            if (Character.isDigit(c)) hasDigit = true;
-            if (c == '@' || c == '.' || c == '!' || c == '_') hasSpecial = true;
-        }
+        
+    //checking password complexity
+        boolean checkPasswordComplexity(String password) {
+            boolean hasUpper = false;
+            boolean hasLower = false;
+            boolean hasDigit = false;
+            boolean hasSpecial = false;
+            for (int i = 0; i < password.length(); i++) {
+                char c = password.charAt(i);
+                if (Character.isUpperCase(c)) hasUpper = true;
+                if (Character.isLowerCase(c)) hasLower = true;
+                if (Character.isDigit(c)) hasDigit = true;
+                if (c == '@' || c == '.' || c == '!' || c == '_') hasSpecial = true;
+            }
         return password.length() >= 8 && hasUpper && hasLower && hasDigit && hasSpecial;
     }
-
-    boolean checkCellPhoneNumber(String number) {
-        boolean startsWithPlus = number.length() > 0 && number.charAt(0) == '+';
-        boolean allDigits = true;
-        for (int i = 1; i < number.length(); i++) {
-            char c = number.charAt(i);
-            if (!Character.isDigit(c)) {
-                allDigits = false;
+        
+        // check cell phone number requirements 
+            boolean checkCellPhoneNumber(String number) {
+                boolean startsWithPlus = number.length() > 0 && number.charAt(0) == '+';
+                boolean allDigits = true;
+                for (int i = 1; i < number.length(); i++) {
+                    char c = number.charAt(i);
+                    if (!Character.isDigit(c)) {
+                        allDigits = false;
+                    }
+                }
+                boolean validLength = (number.length() - 1) <= 10;
+                return startsWithPlus && allDigits && validLength;
             }
-        }
-        boolean validLength = (number.length() - 1) <= 10;
-        return startsWithPlus && allDigits && validLength;
-    }
-
+           
+        //display output message 
     String registerUser(String firstName, String lastName, String username, String password, String cellNumber) {
         boolean usernameOk = checkUserName(username);
         boolean passwordOk = checkPasswordComplexity(password);
